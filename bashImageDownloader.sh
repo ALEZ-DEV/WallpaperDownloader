828313997dbbd208432c797db94b7e2e74d5acf3
ALEZ-DEV <raidersfocus@gmail.com>
2024-07-30 23:07:13 +0200

    update image path

--- bashImageDownloader.sh
+++ bashImageDownloader.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
-images=();
-readarray -t images < ./image.list;
-number=0;
+images=()
+readarray -t images <./image.list
+number=0
+
+if [ ! -d ./saved_images ]; then
+	mkdir -p ./saved_images
+fi
+
 for i in ${!images[@]}; do
-	wget -O $HOME/.config/awesome/theme/assets/images$i.jpg ${images[$i]};
-  number=$i;
-done;
-echo "$number images downloaded !";
+	wget -O ./saved_images/$i.jpg ${images[$i]}
+	number=$i
+done
+echo "$number images downloaded !"
