514088a0fd7d08f6a0dd7ec0d95899cdb68eb56a
ALEZ-DEV <raidersfocus@gmail.com>
2024-07-30 23:19:51 +0200

    updated download implementation

--- bashImageDownloader.sh
+++ bashImageDownloader.sh
@@ -2,13 +2,27 @@
 images=()
 readarray -t images <./image.list
 number=0
+downloaded=0
 
 if [ ! -d ./saved_images ]; then
 	mkdir -p ./saved_images
 fi
 
 for i in ${!images[@]}; do
-	wget -O ./saved_images/$i.jpg ${images[$i]}
+	image_path="./saved_images/$i.jpg"
+
+	if ! [ -e $image_path ]; then
+		wget -O $image_path ${images[$i]}
+		((downloaded++))
+	else
+		echo "Image $i already exist, skipping..."
+	fi
+
 	number=$i
 done
-echo "$number images downloaded !"
+
+if [ $downloaded == 0 ]; then
+	echo "No image has been downloaded"
+else
+	echo "$downloaded images downloaded !"
+fi
