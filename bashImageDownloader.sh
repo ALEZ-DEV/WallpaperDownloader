ca8a7afc94d94466699830a23aea63485b6cdea2
ALEZ-DEV <raidersfocus@gmail.com>
2024-09-26 22:19:26 +0200

    update script

--- bashImageDownloader.sh
+++ bashImageDownloader.sh
@@ -1,18 +1,36 @@
-#!/bin/bash
+#!/bin/sh
+
+missingArgumentErrorMsg="You need to use the command as the following : ./bashImageDownloader.sh <imageFilePath> <outputFolder>"
+
+if [ -z "$1" ]; then
+  echo "<imageFilePath> is the path to the images file to download and it's not set,"
+  echo $missingArgumentErrorMsg
+  exit 1
+fi
+
+if [ -z "$2" ]; then
+  echo "<outputFolder> is the path to the folder output and it's not set,"
+  echo $missingArgumentErrorMsg
+  exit 1
+fi
+
+imageFilePath=$1
+outputFolder=$2
+
 images=()
-readarray -t images <./image.list
+readarray -t images <$imageFilePath
 number=0
 downloaded=0
 
-if [ ! -d ./saved_images ]; then
-	mkdir -p ./saved_images
+if [ ! -d "$outputFolder" ]; then
+	mkdir -p "$outputFolder"
 fi
 
 for i in ${!images[@]}; do
-	image_path="./saved_images/$i.jpg"
+	image_path="$outputFolder/$i.jpg"
 
 	if ! [ -e $image_path ]; then
-		wget -O $image_path ${images[$i]}
+		wget -O "$image_path" "${images[$i]}"
 		((downloaded++))
 	else
 		echo "Image $i already exist, skipping..."
